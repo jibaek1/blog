c2666dce9d278278058c71da2a67a7695922ae81
jibaek <tngus000777@gmail.com>
2025-06-19 11:29:36 +0900

    블로그 리스트 기능 완료

--- src/main/java/com/tenco/blog/Controller/BoardController.java
+++ src/main/java/com/tenco/blog/Controller/BoardController.java
@@ -1,5 +1,82 @@
 package com.tenco.blog.Controller;
 
+
+import com.tenco.blog.model.Board;
+import com.tenco.blog.repository.BoardNativeRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller // IoC 대상 - 싱글톤 패턴으로 관리 됨
 public class BoardController {
+
+    private BoardNativeRepository boardNativeRepository;
+
+    // DI: 의존성 주입 : 스프링이 자동으로 객체를 주입
+    public BoardController(BoardNativeRepository boardNativeRepository) {
+        this.boardNativeRepository = boardNativeRepository;
+    }
+
+
+    // username,title content <--- Dto 받는 방법, 기본 데이터 타입 설정
+    // form 태그에서 넘어오는 데이터 받기
+    // form 태그에 name 속성에 key 값 동일해야 함
+    // http://localhost:8080/board/save
+    // 스프링 부트 기본 파싱 전략 - key=value (form)
+    @PostMapping("/board/save")
+    public String sava(@RequestParam("title") String title,
+                       @RequestParam("content") String content,
+                       @RequestParam("username") String username) {
+        System.out.println("title : " + title);
+        System.out.println("content : " + content);
+        System.out.println("username : " + username);
+
+        boardNativeRepository.save(title,content,username);
+
+        return "redirect:/";
+    }
+
+
+    @GetMapping({"/","/index"})
+//  public String index(Model model) {
+    public String index(HttpServletRequest request) {
+
+        //      prefix: /templates/
+        //      return : index
+        //      suffix : .mustache
+        //      # 기본 경로를 src/main/resources/templates/index.mustache
+
+
+        // DBB 접근해서 select 결과값을 받아서 머스태치 파일에 데이터 바인딩 처리
+        List<Board> boardList = boardNativeRepository.findAll();
+        request.setAttribute("boardList",boardList);
+
+        return "index";
+    }
+
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+        // /templates/board
+        return "board/save-form";
+    }
+
+    /**
+     * 상세보기 화면 요청
+     * board/1
+     */
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable(name = "id") Integer id) {
+        // URL 에서 받은 id 값을 사용해서 특성 게시글 조회
+        // 실제로는 이 id값으로 데이터베이스에 있는 게시글 조회 하고
+        // 머스태치 파일로 데이터를 내려 주어야 함 (Model)
+        return "board/detail";
+    }
+
 
 }
---
+++ src/main/java/com/tenco/blog/repository/BoardNativeRepository.java
@@ -0,0 +1,53 @@
+package com.tenco.blog.repository;
+
+import com.tenco.blog.model.Board;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository // IoC 대상(객체 생성)
+public class BoardNativeRepository {
+
+    // JPA의 핵심 인터페이스
+    // 데이터베이스와의 모든 작업을 담당
+    private EntityManager em;
+
+    // 생성자를 확인해서 자동으로 EntityManager 객체를 주입 시킨다.
+    // DI 처리
+    public BoardNativeRepository(EntityManager em) {
+        this.em = em;
+    }
+    // 트랜잭션 처리
+    @Transactional
+    public void save(String title, String content, String username) {
+        //쿼리기술 --> 네이티브쿼리
+        // Board.class <-- 형변환을 직접적으로 명시하는것이 좋다
+        Query query = em.createNativeQuery("insert into board_tb(title, content, username, created_at) " +
+                " values(?,?,?, now())");
+
+        query.setParameter(1,title);
+        query.setParameter(2,content);
+        query.setParameter(3,username);
+
+        query.executeUpdate();
+
+    }
+
+    // 게시글 목록 조회
+    public List<Board> findAll() {
+        // 쿼리 기술 --> 네이티브 쿼리
+        Query query = em.
+                createNativeQuery("select * from board_tb order by id desc ", Board.class);
+        // query.getResultList() : 여러 행의 결과를 List 객체로 반환
+        // query.getSingleResult() : 단일 결과만 반환 (한 개의 row 데이터만 있을 때)
+//        List<Board> list = query.getResultList();
+        return query.getResultList();//리턴 값을 이렇게 넣어서 코드 간소화
+    }
+
+
+
+
+}
